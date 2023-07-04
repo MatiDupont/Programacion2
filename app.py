55bf1bb62eec0cd0f10631dd83ae66e730220e30
Matias Dupont <114042788+MatiDupont@users.noreply.github.com>
2023-07-04 20:54:32 -0300

    Nuevas agregaciones y algunas modificaciones
    Creacion de funcion "existe_pelicula()"
    Modificacion de funcion "mostrar_peliculas(peliculas)"
    Desarrollo de los 4 primeros endpoints y del ABM solo desarrolle la Alta de la pelicula

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # Crear un programa en Python para mantener una lista de peliculas recomendadas por parte de los usuarios
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response, request
+from http import HTTPStatus
 import json
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
 with open("Proyecto Final/usuarios.json", encoding = "utf-8") as archivo_usuarios:
     usuarios_data = json.load(archivo_usuarios)
@@ -14,6 +15,8 @@ with open("Proyecto Final/peliculas.json", encoding = "utf-8") as archivo_pelicu
 with open("Proyecto Final/directores.json", encoding = "utf-8") as archivo_directores:
     directores_data = json.load(archivo_directores)
     
+flag = False
+
 def mostrar_peliculas(peliculas):
         for pelicula in peliculas[-10:]:
             print("Titulo: " + pelicula["titulo"])
@@ -22,7 +25,105 @@ def mostrar_peliculas(peliculas):
             print("Genero: " + pelicula["genero"])
             print("Sinopsis:" + pelicula["sinopsis"])
             print("---------------------------------")
+            
+def existe_pelicula(titulo):
+    titulo_pelicula = next((pelicula for pelicula in peliculas_data[0]["peliculas"] if pelicula["titulo"].lower() == titulo.lower()),None)
+    if titulo_pelicula:
+        return True
+    else:
+        return False   
+    
+#Ademas, el sistema debera proveer servicios web a traves de endpoints para ser consultados por otros sistemas y devolver un documento de formato JSON.
+#Los servicios que se necesitan son:
+@app.route("/", methods = ["GET"])
+def home():
+    return "Bienvenido al sistema de peliculas 'Film Quest"
 
+# 1. Devolver la lista de directores presentes en la plataforma
+@app.route("/peliculas/directores", methods = ["GET"])
+def devolver_directores():   
+    directores = [director["nombre_director"] for director in directores_data[0]["directores"]]
+    return Response(jsonify(directores), status = HTTPStatus.OK)
+
+# 2. Devolver la lista de generos presentes en la plataforma
+@app.route("/peliculas/generos/todos")
+def devolver_todos_generos():
+    total_generos = [genero["nombre_genero"] for genero in peliculas_data[0]["generos"]]
+    return Response(jsonify(total_generos), methods = HTTPStatus.OK)
+
+@app.route("/peliculas/generos/presentes", methods = ["GET"])
+def devolver_generos():
+    generos = list(set([pelicula["genero"] for pelicula in peliculas_data[0]["peliculas"]]))
+    return Response(jsonify(generos), status = HTTPStatus.OK) 
+
+# 3. Devolver la lista de peliculas dirigidas por un director en particular
+@app.route("/peliculas/directores/<director>", methods = ["GET"])
+def devolver_peliculas_por_director(director):
+    peliculas = [pelicula for pelicula in peliculas_data[0]["peliculas"] if pelicula["director"] == director]
+    return Response(jsonify(peliculas), status = HTTPStatus.OK)
+
+# 4. Devolver las peliculas que tienen imagen de portada agregada
+@app.route("/peliculas/imagen", methods = ["GET"])
+def devolver_peliculas_con_imagen():
+    peliculas_imagen = [pelicula for pelicula in peliculas_data[0]["peliculas"] if pelicula["link"]]
+    return Response(jsonify(peliculas_imagen), status = HTTPStatus.OK)
+
+# 5. ABM de cada pelicula
+@app.route("/peliculas/agregar/nueva", methods = ["POST"])
+def agregar_pelicula():
+    datos_json = request.get_json()
+    
+    campos = {"titulo", "anio", "director", "genero", "sinopsis", "link", "comentarios"}
+    
+    claves = list(next(iter(datos_json)) for clave in range(7))
+    
+    for clave in claves:
+        if (clave == "null"):
+            print("Error!, faltan campos en el pedido.")
+            exit(1)
+            
+    i = 0
+    salida = 0
+    for campo in campos:
+        if (claves[i] == campo):
+            i = i + 1
+            continue
+        else:
+            print("Los campos deben estar en el orden correcto. --> titulo, anio, director, genero, sinopsis, link, comentarios")
+            salida = 1
+            exit(1)
+    
+    if (salida == 0):   
+        if (existe_pelicula(datos_json["titulo"]) == False):
+            id_pelicula = len(peliculas_data[0]["peliculas"]) + 1
+            nueva_pelicula = {
+            "id":id_pelicula,
+            "titulo":datos_json["titulo"],
+            "anio":datos_json["anio"],
+            "director":datos_json["director"],
+            "genero":datos_json["genero"],
+            "sinopsis":datos_json["sinopsis"],
+            "link":datos_json["imagen"],
+            "comentarios":datos_json["comentarios"]
+            }
+        
+            peliculas_data[0]["peliculas"].append(nueva_pelicula)
+        
+            with open("Proyecto Final/peliculas.json", "w", encoding = "utf-8") as archivo_peliculas:
+                json.dump(peliculas_data, archivo_peliculas, indent = 8)
+                print("Se cargo la pelicula nueva con exito.")
+                return Response(jsonify(nueva_pelicula["titulo"]), status = HTTPStatus.OK)
+        else:
+            Response(jsonify("No es posible agregar esa pelicula dado que ya se encuentra registrada en la base de datos del sistema."), status = HTTPStatus.BAD_REQUEST) 
+           
+@app.route("/peliculas/editar", methods = ["PUT"])
+def editar_pelicula():
+    return 0
+
+@app.route("/peliculas/eliminar", methods = ["DELETE"])
+def eliminar_pelicula():
+    return 0
+    
 print("-----------------------------------------------")
 print("BIENVENIDO AL SISTEMA DE PELICULAS 'FILM QUEST'")
 print("-----------------------------------------------")
@@ -46,6 +147,7 @@ else:
     usuario = next((usuario for usuario in usuarios_data[0]["usuarios"] if usuario["user"] == username and usuario["password"] == password), None)
     
     if usuario:
+        flag = True
         print("¡Usuario encontrado en la base de datos!")
         
         while (True):
@@ -73,25 +175,6 @@ else:
                 print("Opcion invalida. Intente nuevamente.")
     else:
         print("Error, el usuario no fue encontrado en la base de datos.")
- 
-    
-#Ademas, el sistema debera proveer servicios web a traves de endpoints para ser consultados por otros sistemas y devolver un documento de formato JSON.
-#Los servicios que se necesitan son:
-#@app.route("/", methods = ["GET"])
-#def home():
-#    return "Bienvenido al sistema de peliculas 'Film Quest"
-# 1. Devolver la lista de directores presentes en la plataforma
-#@app.route("/directores", methods = ["GET"])
-#def devolver_directores():
-#    directores = directores_data[0]["directores"]["nombre_director"]
-#    return jsonify(directores)
-# 2. Devolver la lista de generos presentes en la plataforma
-#@app.route("/peliculas/generos", methods = ["GET"])
-#def devolver_generos():
-#    return jsonify(peliculas_data["genero"])
-# 3. Devolver la lista de peliculas dirigidas por un director en particular
-# 4. Devolver las peliculas que tienen imagen de portada agregada
-# 5. ABM de cada pelicula
 
-#if __name__ == "__main__":
- #   app.run(debug = True)
+if __name__ == "__main__":
+   app.run(debug = True)
